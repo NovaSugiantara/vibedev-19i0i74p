a9aadd31589ab2eada10fd208ca97f8c7d831772
novasugiantara <novasugiantara425@gmail.com>
2026-07-15 01:38:14 +0800

    feat: pre-bundle vue deps in nuxt.config.ts to avoid dev page reloads

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,15 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
+  components: [
+    { path: '~/components', pathPrefix: false },
+  ],
   vite: {
     plugins: [
       tailwindcss(),
     ],
+    optimizeDeps: {
+      include: ['vue', '@vue/runtime-core', '@vue/runtime-dom'],
+    },
   },
 });
